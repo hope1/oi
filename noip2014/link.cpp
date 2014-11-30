2593fd1a6c0a192d5e90bc7bb19f727e0af773a0
hope1 <jeremyxie357@gmail.com>
2014-11-30 15:48:16 +0800

    Update link.cpp

--- noip2014/link.cpp
+++ noip2014/link.cpp
@@ -1,4 +1,4 @@
-// 0/10
+// 10/10
 #include <cstdio>
 #include <iostream>
 #include <cassert>
@@ -14,34 +14,18 @@ void redirect() {
 }
 
 #define MAX_N   200000
-#define MOD     10007
+#define M       10007
 int N, W[MAX_N + 1];
-int root, nil;
 vector<int> G[MAX_N + 1];
-int f_tab[MAX_N + 1][3], g_tab[MAX_N + 1][3];
 
-int f(int u, int k) {
-    if(k == 0)
-        return W[u];
-
-    if(f_tab[u][k] < 0)
-        for(size_t i = 0; i < G[u].size(); ++i)
-            f_tab[u][k] = max(f_tab[u][k], f(G[u][i], k - 1));
-
-    return f_tab[u][k];
+bool cmpgt_w(int lhs, int rhs) {
+    return W[lhs] > W[rhs];
 }
 
-int g(int u, int k) {
-    if(k == 0)
-        return W[u];
-
-    if(g_tab[u][k] < 0) {
-        g_tab[u][k] = 0;
-        for(size_t i = 0; i < G[u].size(); ++i)
-            g_tab[u][k] = (g_tab[u][k] + g(G[u][i], k - 1)) % MOD;
-    }
-
-    return g_tab[u][k];
+int mod(int a, int m) {
+    if(a < 0)
+        a += ((-a / m) + 2) * m;
+    return a % m;
 }
 
 void input() {
@@ -60,33 +44,27 @@ void input() {
         cin >> W[i];
 }
 
-void build_tree(int r, int p) {
-    for(size_t i = 0; i < G[r].size(); ++i) {
-        if(G[r][i] == p)
-            G[r].erase(G[r].begin() + i--);
-        else
-            build_tree(G[r][i], r);
-    }
-}
-
 int main() {
     input();
 
-    nil = 0;
-    root = 1;
-    build_tree(root, nil);
+    int wmax = 0, wsum = 0;
+    for(int i = 1; i <= N; ++i) {
+        if(G[i].size() >= 2) {
+            partial_sort(G[i].begin(), G[i].begin() + 2, G[i].end(), cmpgt_w);
+            wmax = max(wmax, (W[G[i][0]] % M) * (W[G[i][1]] % M));
 
-    for(int u = 1; u <= N; ++u)
-    for(int k = 0; k <= 2; ++k)
-        f_tab[u][k] = -1, g_tab[u][k] = -1;
-
-    int ans1 = 0, ans2 = 0;
-    for(int u = 1; u <= N; ++u) {
-        ans1 = max(ans1, f(u, 2) * W[u]);
-        ans2 = (ans2 + g(u, 2) * W[u]) % MOD;
+            //   sum w(u, v)
+            // = 2 * sum (w(u, v) [u < v])
+            // = (sum Wu)^2 - sum (Wu^2)
+            int sum = 0, sqrsum = 0;
+            for(int k = 0; k < G[i].size(); ++k) {
+                sum = mod(sum + W[G[i][k]], M);
+                sqrsum = mod(sqrsum + W[G[i][k]] * W[G[i][k]], M);
+            }
+            wsum = mod(wsum + sum * sum - sqrsum, M);
+        }
     }
-    ans2 = (ans2 * 2) % MOD;
 
-    cout << ans1 << " " << ans2 << endl;
+    cout << wmax << " " << wsum << endl;
     return 0;
 }
