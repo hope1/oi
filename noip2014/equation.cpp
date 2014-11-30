88c6b8101db092f4574329c30c1ba407278c5db6
hope1 <jeremyxie357@gmail.com>
2014-11-30 17:06:00 +0800

    Update equation.cpp

--- noip2014/equation.cpp
+++ noip2014/equation.cpp
@@ -1,4 +1,4 @@
-// 7/10
+// 10/10
 #include <cstdio>
 #include <iostream>
 #include <cassert>
@@ -7,6 +7,7 @@
 #include <cstring>
 #include <ctime>
 #include <cstdlib>
+#include <functional>
 using namespace std;
 
 #ifdef Mdebug
@@ -22,8 +23,9 @@ void redirect() {
     ios::sync_with_stdio(false);
 }
 
-#define MAX_PRIME   10000
-#define MAX_NPRIME  2000
+#define MAX_PRIME   30000
+#define MIN_PRIME   1000
+#define MAX_NPRIME  5000
 int prime[MAX_NPRIME], nprime;
 void sieve(int n) {
     static bool tab[MAX_PRIME + 1];
@@ -41,7 +43,8 @@ void sieve(int n) {
 
 void generate_prime(int n, int k) {
     sieve(n);
-    random_shuffle(prime, prime + nprime);
+    int *it = remove_if(prime, prime + nprime, bind2nd(less<int>(), MIN_PRIME));
+    random_shuffle(prime, it);
     nprime = min(nprime, k);
 }
 
@@ -52,23 +55,27 @@ int mod(long long a, int p) {
     return a % p;
 }
 
-#define K       20
+#define K       10
 #define MAX_N   100
 #define MAX_LEN 10000
 int n, m, a_len[MAX_N + 1];
 char a[MAX_N + 1][MAX_LEN + 10];
 int b[K][MAX_N + 1];
+int r[K][MAX_PRIME + 1];
 
-int eval(int *c, int p, int x) {
-    long long r = c[n];
-    for(int k = n - 1; k >= 0; --k)
-        r = mod(r * x + c[k], p);
-    return r;
+int eval(int i, int x) {
+    if(r[i][x] < 0) {
+        long long z = b[i][n];
+        for(int k = n - 1; k >= 0; --k)
+            z = mod(z * x + b[i][k], prime[i]);
+        r[i][x] = z;
+    }
+    return r[i][x];
 }
 
 bool test(int x) {
     for(int i = 0; i < nprime; ++i)
-        if(eval(b[i], prime[i], x % prime[i]) != 0)
+        if(eval(i, x % prime[i]) != 0)  // f(x + kp) = f(x) (mod p)
             return false;
     return true;
 }
@@ -93,6 +100,10 @@ int main() {
                 b[i][k] = mod(-b[i][k], prime[i]);
         }
     }
+
+    for(int i = 0; i < nprime; ++i)
+        for(int x = 0; x <= MAX_PRIME; ++x)
+            r[i][x] = -1;
 
     vector<int> root;
     for(int x = 1; x <= m; ++x)
